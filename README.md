edf7a7309faac0f06e0eed1845ca7dac53fd1a02
michaelballos <mdb3091207@gmail.com>
2022-03-23 23:20:45 -0500

    Button Counter Add:

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-#README
-
-This is a react redo of previous button counter project I originally did using vanilla JavaScript
-
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Imagebackground from './img/Imagebackground.svg';
+
+function Title({ title }) {
+  return <p id='interfaceTitle'>{title}</p>;
+}
+
+
 
 function App() {
+  const [currentCount, countFunc] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <img id="bg" src={Imagebackground} />
+
+      <div className="App">
+        <p id="pageTitle">Į̶̛̾̓̿̃̃͌̎͊͝ͅn̶̪̼̥̖͖̄̉́͐̔̈́͂͊̈́͜͝c̵͉̭͓̘̯̩̝̜̘̦͙̖̼̽̄̋̒r̵̻̥̘͈̣̻̩̎̅́e̵̲̲̝̤̊̑̄ḿ̷̢̢̨̡̟̯̠͚͇̈́e̴̠̣̝̩͍͈͉̜̐̉́͒͑͒̿̓͑͊̊͝ͅṉ̴̨̮̰̼̹̺͈̞̙͂̓ͅt̷̢̧̛͚̙͆͆͒́̈́̒͌̆̏̀ ̵̨̨̛͚̥̻͖̺̙̜͍̭͙̥̈́̃̈́̍̐a̶̧̞͖͈̖̖͂̆́̍͛̌̄͊̔͗͐̕͠n̴̬̻̹̉́̍̌̒̈́̋̏̎̏̍̾͠d̷̡̨̜͔̳̰̮̜̾̔́͑̌̊̃͐̽́̑̓͠ ̵̡̧̹̬͙͍͇͂̒̈́̓̂̑̊̋̈́͌͘͜͝Ḑ̷̡̞̣͖̼̳̞̹̲̪͌̌ͅe̶̢̝͇̩̦̘̖̱̯̮̅͗́͌̀̅̿̂̓͝͝ͅͅc̷̞̻̃̇͛̓́͘r̵͈̮̽͆͋̚͝ě̵̻̞̦̏̌̄̔̅̕͝m̶̨͚̘̟̦͚̫͈̤̝̝̈͂̐̐̆̓̃̾͑̀̅͗͠ͅȩ̸̥͉̝̣̬̞̮̲̟̃̍͋̂̀͗̐̕ͅň̴̝̫̣͋̅̌͊̾̕t̷̬̩̜̣̪͎̬͈̱͋̔̄̽͘̕ͅ ̷̡̪̜̿̎̃̏̒̾̕̕͜͝͝͝C̶̡̻̖̲̱͈̏̓̋͗̒̇̓͌̕͘͝ͅȍ̷̡̖̖̩͕̪͈͉͈̊̎̍̌̈́̿ͅͅͅü̶͔̅̿̈́̇̏͂̄ń̶̖͕͈̂͗̂̂͐̏͂̔̓̏̊͝t̵̨̢͔͖͋̐̏͊͌̋̇̌̕̕͘e̵͇͙̖͚̥̦͂͗̇̊̄͐̓̂̒̽͝͝͝ͅr̷̩̖̤͇͙̞̪̖̽̄͑</p>
+
+        <div id="counterInterface">
+          <Title title="Press Me Harder rawr XD" />
+          <p id='cunt'>{currentCount}</p>
+
+          <p id='countLabel'>Current Count:</p>
+          <div id="buttons">
+            <button id="sub" onClick={() => countFunc(currentCount - 1)}>
+              Subtract
+            </button>
+            <button id="add" onClick={() => countFunc(currentCount + 1)}>
+              Add
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
